1bcd704795f04dc2afd9fd93cc27951665c7533a
Leonardo <leonardo.ghiggino@calindra.com.br>
2026-09-09 21:44:57 -0300

    refactor: altera forceWeb para que resolve patterns de path dinamicos

--- src/services/DeeplinkResolver.js
+++ src/services/DeeplinkResolver.js
@@ -140,16 +140,6 @@ const resolveDeeplinkToProductCatalog = deeplink => {
 	}
 }
 
-const resolveShareLink = deeplink => {
-	// A pedido da montecarlo - essa rota estava caindo no ultimo resolver a abrindo um PLP de forma errônea
-	console.log('resolveShareLink')
-	if (deeplink.includes('/api/io/_v/share/')) {
-		openRedirectLinkBrowser(deeplink)
-		return true
-	}
-	return false
-}
-
 const resolveStoreLinks = deeplink => {
 	console.log('resolveStoreLinks')
 	if (deeplink.includes('play.google') || deeplink.includes('app.apple')) {
@@ -172,7 +162,11 @@ export const resolveDeeplinkFromRemoteConfig = deeplink => {
 
 	if (matchedDeeplink) {
 		if (matchedDeeplink?.forceWeb) {
-			openWebFlow(matchedDeeplink?.forceWeb)
+			if (matchedDeeplink.forceWeb === true) {
+				openRedirectLinkBrowser(deeplink)
+			} else {
+				openWebFlow(matchedDeeplink.forceWeb)
+			}
 			return true
 		}
 
@@ -233,7 +227,6 @@ export const resolveDeeplinkPath = async deeplink => {
 		resolveDeeplinkToProduct,
 		resolveDeeplinkFromRemoteConfig,
 		resolveDeeplinkLandingPage,
-		resolveShareLink,
 		resolveDeeplinkToProductCatalog,
 		openRedirectLinkBrowser
 	]
